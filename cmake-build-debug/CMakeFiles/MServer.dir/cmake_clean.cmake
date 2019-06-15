708d154e469b23d30e7ce8732d580e8afb9cce6e
sunwenfeng <2655696423@qq.com>
2019-06-15 14:16:41 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/MServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MServer.dir/main.cpp.o"
+  "MServer.pdb"
+  "MServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
