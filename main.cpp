78987f1fd9e4552cf1eeaa6bf4dd285abdc8141c
user.email <sun@localhost.localdomain>
2019-07-03 10:38:23 +0800

    third version

--- main.cpp
+++ main.cpp
@@ -7,12 +7,16 @@ void message(){
     std::cout<<"receive one message"<<std::endl;
     //write(5,"hello",sizeof("hello"));
 }
+/*
+ * 现在有一个问题就是参考muduo的方式定义业务处理毁掉函数时出错，在目前的测试中虽然定义了回调函数，但在Tcp::connection的handlread中没有使用回调函数
+ *
+ *
+ * */
 int main() {
     EventLoop loop;
     TcpServer server(&loop,2008);
     server.setTcpServerReadCallback(std::bind(message));
     server.start();
     loop.loop();
-
     return 0;
 }
